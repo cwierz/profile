fdea3d7ac3f0af9795c3a63f695dceff73aa3ff2
Catherine Wierzba <catherinewierzba@MacBook-Pro-6.local>
2021-02-09 13:40:49 -0800

    Add second pic

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <p>
           Hi Jesse pls don't me mad at me thank u
         </p>
+        <img src="https://www.linkedin.com/in/jessedfordyce/detail/photo/" alt="Pic"/>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
