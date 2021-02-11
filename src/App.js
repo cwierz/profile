3a9650d41ab0882f8cce1cedba07050fc80870ba
Catherine Wierzba <catherinewierzba@MacBook-Pro-6.local>
2021-02-11 15:11:32 -0800

    Adding pics, removing spinning thing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import sweater from './imgs/pic.png';
 
 function App() {
+  const cards = [{
+    title: '  ',
+    img: sweater
+  }, {
+    title: '  ',
+    img: sweater
+  }]
+
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Hi Jesse pls don't be mad at me thank u
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="Container">
+          {cards.map(card =>
+            <div className="Card">
+              <img src={card.img} alt="Pic" style={{
+                maxWidth: '250px',
+                height: 'auto',
+                display: 'block',
+                // marginLeft: 'auto',
+                // marginRight: 'auto'
+              }} />
+              {/* <div className="Title">
+                {card.title}
+              </div> */}
+            </div>)}
+        </div>
+
       </header>
       <header className="App-subheader">
-      <p>
-        do u like my pink website?
+        <p>
+          do u like my pink website?
       </p>
-      <p>
-        I'm still upset u gave me an F
+        <p>
+          I'm still upset u gave me an F
       </p>
-    </header>
+      </header>
     </div>
   );
 }
